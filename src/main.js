0c588bc5e0ea6edc8562c8be50a9e3dfd5cd1bbf
박예찬 <jkyyc3@gmail.com>
2022-01-26 22:52:59 +0900

    CREATE: Vue Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './routes/index.js';
 
-createApp(App).mount('#app')
+// use 메소드는 플러그인을 적용하고자 할 때 사용
+createApp(App).use(router).mount('#app');
--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,3 @@
 <template>
-  <RouterView />
+  <div>About!</div>
 </template>
