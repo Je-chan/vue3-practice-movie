93b9bd78e9f1d70da891eb652481008e420cc23f
박예찬 <jkyyc3@gmail.com>
2022-01-29 21:38:30 +0900

    CREATE : Head-line

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,4 +1,12 @@
 <template>
-  <h1>Home</h1>
-  <div class="btn btn-primary">Home!</div>
+  <HeadLine />
 </template>
+
+<script>
+import HeadLine from '~/components/HeadLine';
+export default {
+  components: {
+    HeadLine,
+  },
+};
+</script>
