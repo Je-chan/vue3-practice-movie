0f5e9b44032d72ff26f21d14ab05aecbb4e0d8af
박예찬 <jkyyc3@gmail.com>
2022-01-29 22:36:42 +0900

    CREATE : Search & filter

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <HeadLine />
+  <Search />
 </template>
 
 <script>
-import HeadLine from '~/components/HeadLine';
+import HeadLine from "~/components/HeadLine";
+import Search from "~/components/Search";
 export default {
   components: {
     HeadLine,
+    Search,
   },
 };
 </script>
