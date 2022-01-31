924f21f96abd4723602c8a9877fc56e066d6518c
박예찬 <jkyyc3@gmail.com>
2022-02-01 01:14:37 +0900

    CREATE : 작성자 About 페이지 작성

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,84 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader v-if="imageLoading" absolute />
+      <img :src="image" :alt="name" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
+
+<script>
+import Loader from '~/components/Loader';
+export default {
+  data() {
+    return {
+      imageLoading: true,
+    };
+  },
+  components: {
+    Loader,
+  },
+  computed: {
+    image() {
+      return this.$store.state.about.image;
+    },
+    name() {
+      return this.$store.state.about.name;
+    },
+    email() {
+      return this.$store.state.about.email;
+    },
+    blog() {
+      return this.$store.state.about.blog;
+    },
+    phone() {
+      return this.$store.state.about.phone;
+    },
+  },
+  // 라이프사이클에서는 별도로 async 를 달 수 없으므로 별도의 methods 를 생성하는 것ㄴ
+  mounted() {
+    this.init();
+  },
+
+  methods: {
+    async init() {
+      await this.$loadImage(this.image);
+      this.imageLoading = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~/scss/main';
+.about {
+  text-align: center;
+  .photo {
+    position: relative;
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    img {
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+
+  .name {
+    font-size: 40px;
+    font-family: 'Oswald', sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
