8d01b7e2642e9b1f412c3627d0bc629f300af774
박예찬 <jkyyc3@gmail.com>
2022-01-31 21:07:27 +0900

    UPDATE : Movie 페이지 스타일 업데이트

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,7 +25,7 @@ export default {
         },
         {
           name: 'Movie',
-          href: '/movie/4520988',
+          href: '/movie/tt4520988',
         },
         {
           name: 'About',
--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,17 +1,69 @@
 <template>
   <div class="container">
-    <div class="skeletons">
-      <div class="skeleton poster"></div>
+    <template v-if="loading">
+      <div class="skeletons">
+        <div class="skeleton poster"></div>
+        <div class="specs">
+          <div class="skeleton title"></div>
+          <div class="skeleton spec"></div>
+          <div class="skeleton plot"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+        </div>
+      </div>
+      <Loader :size="3" :z-index="9" fixed />
+    </template>
+    <div v-else class="movie-details">
+      <div
+        :style="{
+          backgroundImage: `url(${requestDiffSizeImage(theMovie.Poster)})`,
+        }"
+        class="poster"
+      ></div>
       <div class="specs">
-        <div class="skeleton title"></div>
-        <div class="skeleton spec"></div>
-        <div class="skeleton plot"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
+        <div class="title">
+          {{ theMovie.Title }}
+        </div>
+        <div class="labels">
+          <span> {{ theMovie.Released }}</span>
+          <span> {{ theMovie.Runtime }}</span>
+          <span> {{ theMovie.Country }}</span>
+        </div>
+        <div class="plot">
+          {{ theMovie.Plot }}
+        </div>
+        <div class="ratings">
+          <h3>Ratings</h3>
+          <div class="rating-wrap">
+            <div
+              v-for="{ Source: name, Value: score } in theMovie.Ratings"
+              :key="name"
+              :title="name"
+              class="rating"
+            >
+              <img
+                :src="`https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/${name}.png`"
+                :alt="name"
+              />
+              <span>{{ score }}</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Actors</h3>
+          {{ theMovie.Actors }}
+        </div>
+        <div>
+          <h3>Director</h3>
+          {{ theMovie.Director }}
+        </div>
+        <div>
+          <h3>Genre</h3>
+          {{ theMovie.Genre }}
+        </div>
       </div>
     </div>
-    <Loader :size="3" :z-index="9" fixed />
   </div>
 </template>
 <script>
@@ -21,6 +73,14 @@ export default {
   components: {
     Loader,
   },
+  computed: {
+    theMovie() {
+      return this.$store.state.movie.theMovie;
+    },
+    loading() {
+      return this.$store.state.movie.loading;
+    },
+  },
   created() {
     // console.log(this.$route);
     // fullPath, href, params: {id: ~~} 등등의 정보가 들어가 있음
@@ -28,6 +88,11 @@ export default {
       // path 의 params 를 가져오는 것
       id: this.$route.params.id,
     });
+  },
+  methods: {
+    requestDiffSizeImage(url, size = 700) {
+      return url.replace('SX300', `SX${size}`);
+    },
   },
 };
 </script>
@@ -68,6 +133,67 @@ export default {
       width: 50%;
       height: 50px;
       margin-top: 20px;
+    }
+  }
+}
+.movie-details {
+  display: flex;
+  color: $gray-600;
+  .poster {
+    width: 500px;
+    height: 500px * 3 / 2;
+    margin-right: 70px;
+    border-radius: 10px;
+    background-color: $gray-200;
+    background-size: cover;
+    background-position: center;
+    flex-shrink: 0;
+  }
+  .specs {
+    flex-grow: 1;
+    .title {
+      color: $black;
+      font-family: 'Oswald', sans-serif;
+      font-size: 70px;
+      line-height: 1;
+      margin-bottom: 30px;
+    }
+    .labels {
+      color: $primary;
+      // http://www.evotech.net/blog/2007/04/named-html-entities-in-numeric-order/
+      span {
+        &::after {
+          content: '\00b7';
+          margin: 0 6px;
+        }
+        &:last-child::after {
+          display: none;
+        }
+      }
+    }
+    .plot {
+      margin-top: 20px;
+    }
+    .ratings {
+      .rating-wrap {
+        display: flex;
+        .rating {
+          display: flex;
+          align-items: center;
+          margin-right: 32px;
+          img {
+            height: 30px;
+            flex-shrink: 0;
+            margin-right: 6px;
+          }
+        }
+      }
+    }
+    h3 {
+      margin: 24px 0 6px;
+      color: $black;
+      font-family: 'Oswald', sans-serif;
+      font-size: 20px;
     }
   }
 }
